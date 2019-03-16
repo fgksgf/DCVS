1cb98da0c2315bf4aac8164776731b16fad1b0f0
fgksgf <fgksgf@yahoo.com>
2019-03-16 16:53:17 +0800

    完善数据库工具类

--- util/mongo_util.py
+++ util/mongo_util.py
@@ -1,12 +1,10 @@
 import pymongo
 
+from model.jd.product import Product
+
 client = pymongo.MongoClient('mongodb://localhost:27017/')
-collection = client['jd'].products
 
 
-def find_one_by_pid(pid):
-    return collection.find_one({"pid": str(pid)})
-
-
-if __name__ == '__main__':
-    print(find_one_by_pid(100000822969))
+def get_product_by_pid(pid):
+    collection = client['jd'].products
+    return Product(collection.find_one({"pid": str(pid)}))
