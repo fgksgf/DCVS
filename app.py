747a89111a4b737b15d91c9d0910ecd87b74104d
fgksgf <fgksgf@yahoo.com>
2019-03-20 16:38:40 +0800

    增加数据可视化接口

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import json
-import random
-
-# from pyecharts import Scatter3D
 import time
-
 from flask import Flask, render_template
 
-from util.mongo_util import find_one_by_pid
+from service.jd_service import JDPage
+from util.mongo_util import get_product_by_pid
 from util.redis_util import redis_client
 
 app = Flask(__name__)
 REMOTE_HOST = "https://pyecharts.github.io/assets/js"
 PREFIX = 'https://item.jd.com/'
+POSTFIX = '.html'
+WORK_LOAD = 150
 
 
 @app.route('/')
@@ -21,60 +20,48 @@ def index():
 
 @app.route('/crawl/jd/<string:pid>', methods=['GET'])
 def jd_crawler(pid):
-    # eg. pid: '100000822969.html'
-    # 将获取的pid与网址前缀连接，插入到任务队列
-    redis_client.lpush('jd:items_urls', PREFIX + pid)
-    # eg. pid: '100000822969'
+    # eg. pid: '100000822969.html', 去除后缀
     pid = pid[:-5]
+    # 查询数据库中是否已存在该商品
+    product = get_product_by_pid(pid)
+    if product is None:
+        # if len(product.comments) = 3000:
+        # 若无，将获取的pid与网址前后缀连接，插入到任务队列
+        redis_client.lpush('jd:items_urls', PREFIX + pid + POSTFIX)
 
-    # 每两秒查询一次，爬取任务是否完成
-    while int(redis_client.get(pid)) > 0:
+        # 每两秒查询一次，爬取任务是否完成
+        while int(redis_client.get(pid)) > 0:
+            time.sleep(2)
+    elif len(product.comments) < WORK_LOAD:
+        # 说明当前爬取任务正在进行中
         time.sleep(2)
 
     result = {'result': 'ok', 'pid': pid}
     return json.dumps(result)
 
 
-@app.route('/analyze/jd/<string:pid>')
-def jd_analyze(pid):
-    product = find_one_by_pid(pid)
+@app.route('/analyze/jd/<string:chart>/<string:pid>', methods=['GET'])
+def jd_charts(chart, pid):
+    product = get_product_by_pid(pid)
+    jd_page = JDPage(product)
+    chart_type = 1
+    if chart == 'bar':
+        jd_page.generate_bar_or_pie_charts(bar=True)
+    elif chart == 'pie':
+        jd_page.generate_pie_charts()
+        chart_type = 2
+    elif chart == 'wordcloud':
+        jd_page.generate_word_cloud_charts()
+        chart_type = 3
 
-
-#
-# @app.route('/chart')
-# def hello():
-#     s3d = scatter3d()
-#     return render_template(
-#         "chart.html",
-#         myechart=s3d.render_embed(),
-#         host=REMOTE_HOST,
-#         script_list=s3d.get_js_dependencies(),
-#     )
-#
-#
-# def scatter3d():
-#     data = [generate_3d_random_point() for _ in range(80)]
-#     range_color = [
-#         "#313695",
-#         "#4575b4",
-#         "#74add1",
-#         "#abd9e9",
-#         "#e0f3f8",
-#         "#fee090",
-#         "#fdae61",
-#         "#f46d43",
-#         "#d73027",
-#         "#a50026",
-#     ]
-#     scatter3D = Scatter3D("3D scattering plot demo", width=1200, height=600)
-#     scatter3D.add("", data, is_visualmap=True, visual_range_color=range_color)
-#     return scatter3D
-#
-#
-# def generate_3d_random_point():
-#     return [
-#         random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)
-#     ]
+    return render_template(
+        "dashboard.html",
+        pid=pid,
+        chart_type=chart_type,
+        myechart=jd_page.page.render_embed(),
+        host=REMOTE_HOST,
+        script_list=jd_page.page.get_js_dependencies()
+    )
 
 
 if __name__ == '__main__':
