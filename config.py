c9d14b177449d69e81fb6926797d6f6648e51e32
fgksgf <fgksgf@yahoo.com>
2019-03-28 17:41:51 +0800

    add:配置类

---
+++ config.py
@@ -0,0 +1,15 @@
+# app.py config
+REMOTE_HOST = "https://pyecharts.github.io/assets/js"
+PREFIX = 'https://item.jd.com/'
+POSTFIX = '.html'
+WORK_LOAD = 150
+
+# redis config
+REDIS_HOST = '39.96.94.81'
+REDIS_PORT = 6379
+REDIS_PASSWORD = 'iE23fP$d9yTQCgmc'
+
+
+class Config:
+    # flask config
+    SECRET_KEY = 'you-will-never-guess'
