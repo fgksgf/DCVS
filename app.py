8c7555e780c5605cd199de17cae8b2048981c618
fgksgf <fgksgf@yahoo.com>
2019-03-28 17:41:26 +0800

    refactor:配置从配置类中读取

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-import io
 import json
 import time
-from io import BytesIO, StringIO
 
-from flask import Flask, render_template, make_response, session, send_file
+from flask import Flask, render_template, session, send_file, request
 
+from config import *
 from service.jd_service import JDPage
 from util.code_util import generate_verify_image
 from util.mongo_util import get_product_by_pid
 from util.redis_util import redis_client
 
 app = Flask(__name__)
-app.secret_key = "super secret key"
-REMOTE_HOST = "https://pyecharts.github.io/assets/js"
-PREFIX = 'https://item.jd.com/'
-POSTFIX = '.html'
-WORK_LOAD = 150
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -23,23 +18,29 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/crawl/jd/<string:pid>', methods=['GET'])
-def jd_crawler(pid):
-    # eg. pid: '100000822969.html', 去除后缀
-    pid = pid[:-5]
+@app.route('/crawl/jd/', methods=['POST'])
+def jd_crawler():
+    pid = request.values.get('pid', 0)
+    code = request.values.get('code', 0)
+
+    # 判断验证码是否正确
+    if code != session.get('code'):
+        result = {'result': 'wrong_code'}
+        return json.dumps(result)
+
     # 查询数据库中是否已存在该商品
     product = get_product_by_pid(pid)
     if product is None:
-        # if len(product.comments) = 3000:
         # 若无，将获取的pid与网址前后缀连接，插入到任务队列
         redis_client.lpush('jd:items_urls', PREFIX + pid + POSTFIX)
-
         # 每两秒查询一次，爬取任务是否完成
         while int(redis_client.get(pid)) > 0:
             time.sleep(2)
     elif len(product.comments) < WORK_LOAD:
         # 说明当前爬取任务正在进行中
-        time.sleep(2)
+        # 每两秒查询一次，爬取任务是否完成
+        while int(redis_client.get(pid)) > 0:
+            time.sleep(2)
 
     result = {'result': 'ok', 'pid': pid}
     return json.dumps(result)
@@ -48,16 +49,20 @@ def jd_crawler(pid):
 @app.route('/analyze/jd/<string:chart>/<string:pid>', methods=['GET'])
 def jd_charts(chart, pid):
     product = get_product_by_pid(pid)
+    if product is None:
+        return '404!!!'
     jd_page = JDPage(product)
     chart_type = 1
     if chart == 'bar':
-        jd_page.generate_bar_or_pie_charts(bar=True)
+        jd_page.generate_stacked_bar_charts()
     elif chart == 'pie':
         jd_page.generate_pie_charts()
         chart_type = 2
     elif chart == 'wordcloud':
         jd_page.generate_word_cloud_charts()
         chart_type = 3
+    else:
+        return '404!'
 
     return render_template(
         "dashboard.html",
@@ -74,10 +79,9 @@ def get_code():
     image, code = generate_verify_image()
     fp = './static/img/' + code + '.png'
     image.save(fp, 'png')
-    session['image'] = code
+    session['code'] = code
     return send_file(fp, mimetype='image/png')
-    # return response
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
