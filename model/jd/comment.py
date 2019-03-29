3873d8c9e93c5ed1fb20a28ebe244ffb8193fc62
fgksgf <fgksgf@yahoo.com>
2019-03-29 16:19:21 +0800

    refactor：处理购买后多少天评论字段

--- model/jd/comment.py
+++ model/jd/comment.py
@@ -1,13 +1,33 @@
+def convert_day_to_str(day):
+    """
+    将天数转换为对应的文字
+
+    :param day: 天数
+    :return: 对应的字符串
+    """
+    day = int(day)
+    lst = ['0-10天', '11-20天', '21-30天', '31-60天', '60天后']
+    if day > 60:
+        index = -1
+    elif day >= 31:
+        index = -2
+    elif day >= 21:
+        index = -3
+    elif day >= 11:
+        index = -4
+    else:
+        index = -5
+    return lst[index]
+
+
 class Comment:
     def __init__(self, item):
         # 评论内容, 删除评价中的无关字符'hellip'与首尾空格
         self.content = str(item.get('content')).strip().replace('hellip', '')
         # 评分：1为差评，2-3为中评，4-5为好评
         self.score = int(item.get('score'))
-        # 是否使用移动端进行评论
-        # self.is_mobile = (item.get('isMobile') == 'true')
         # 购买后多少天评论
-        self.after_days = int(item.get('days'))
+        self.after_days = convert_day_to_str(item.get('days'))
         # 商品颜色
         self.product_color = str(item.get('productColor'))
         # 商品配置
