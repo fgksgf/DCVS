e6a3bd4de11741c74f4017a97dca94d6fa34359b
fgksgf <fgksgf@yahoo.com>
2019-03-29 16:19:42 +0800

    add：购买后多少天评论饼图和条形图

--- service/jd_service.py
+++ service/jd_service.py
@@ -6,20 +6,20 @@ from util.db_util import get_product_by_pid
 
 
 class JDPage:
+
     def __init__(self, product):
         self.page = Page()
         self.product = product
+        self.attr_title_dict = {'product_size': "不同配置购买量", 'product_color': "不同颜色购买量",
+                                'level': "用户等级", 'client': "用户客户端", 'after_days': '购买多少天后评论'}
 
     def generate_stacked_bar_charts(self):
         """
         生成不同配置购买量、不同颜色购买量、用户等级和用户客户端的层叠条形图
         """
-        attr_title_dict = {'product_size': "不同配置购买量", 'product_color': "不同颜色购买量",
-                           'level': "用户等级", 'client': "用户客户端"}
         titles = ['好评', '中评', '差评']
-        for attr in attr_title_dict.keys():
+        for attr in self.attr_title_dict.keys():
             d = {}
-            a = []
             for c in self.product.comments:
                 temp = getattr(c, attr)
                 if c.score > 3:
@@ -36,14 +36,23 @@ class JDPage:
                     d[temp][index] = 1
 
             v = [[], [], []]
-            for k in d.keys():
-                if k == '':
-                    a.append('其他')
-                else:
-                    a.append(k)
-                for i in range(3):
-                    v[i].append(d.get(k)[i])
-            chart = Bar(attr_title_dict.get(attr))
+            if attr == 'after_days':
+                # 使得x轴名称能按照从少到多的顺序
+                a = ['0-10天', '11-20天', '21-30天', '31-60天', '60天后']
+                for k in a:
+                    for i in range(3):
+                        v[i].append(d.get(k)[i])
+            else:
+                a = []
+                for k in d.keys():
+                    if k == '':
+                        a.append('网页')
+                    else:
+                        a.append(k)
+                    for i in range(3):
+                        v[i].append(d.get(k)[i])
+
+            chart = Bar(self.attr_title_dict.get(attr))
             for i in range(3):
                 chart.add(titles[i], a, v[i], xaxis_interval=0, is_stack=True)
             self.page.add(chart)
@@ -73,9 +82,7 @@ class JDPage:
         self.page.add(chart)
 
         # 生成不同配置购买量、不同颜色购买量、用户等级和用户客户端的饼图
-        attr_title_dict = {'product_size': "不同配置购买量", 'product_color': "不同颜色购买量",
-                           'level': "", 'client': "用户客户端"}
-        for attr in attr_title_dict.keys():
+        for attr in self.attr_title_dict.keys():
             d = {}
             for c in self.product.comments:
                 temp = getattr(c, attr)
@@ -87,12 +94,17 @@ class JDPage:
             v = []
             for k in d.keys():
                 if k == '':
-                    a.append('其他')
+                    a.append('网页')
                 else:
                     a.append(k)
                 v.append(d.get(k))
 
-            chart = Pie(attr_title_dict.get(attr))
+            if attr == 'level':
+                # 解决图例过长遮挡标题的bug
+                chart = Pie('')
+            else:
+                chart = Pie(self.attr_title_dict.get(attr))
+
             chart.add("", a, v, is_label_show=True)
             self.page.add(chart)
 
@@ -120,8 +132,9 @@ class JDPage:
 if __name__ == '__main__':
     prod = get_product_by_pid(100000822981)
     p = JDPage(prod)
+    # p.foo()
     # p.generate_stacked_bar_charts()
-    p.generate_word_cloud_charts()
-    # p.generate_pie_charts()
+    # p.generate_word_cloud_charts()
+    p.generate_pie_charts()
     # p.generate_bar_chart_about()
     p.page.render()
