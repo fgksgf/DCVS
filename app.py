98ae6e90f4c3b64c4670852b59a3778f6ad32fd8
Hoshea <fgksgf@gmail.com>
2020-02-17 21:32:13 +0800

    delete captcha

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import json
 import time
 
-from flask import Flask, render_template, session, send_file, request
+from flask import Flask, render_template, request
+from jinja2 import Markup, Environment, FileSystemLoader
+from pyecharts.globals import CurrentConfig
 
 from config import *
 from service.jd_service import JDPage
-from util.code_util import *
 from util.db_util import get_product_by_pid, redis_client
+
+# CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./templates"))
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 
 
 @app.route('/')
@@ -20,13 +24,6 @@ def index():
 @app.route('/crawl/jd/', methods=['POST'])
 def jd_crawler():
     pid = request.values.get('pid', 0)
-    # 验证码忽略大小写
-    code = str(request.values.get('captcha', 0)).lower()
-
-    # 判断验证码是否正确
-    if code != session.get('captcha'):
-        result = {'result': 'wrong_code'}
-        return json.dumps(result)
 
     # 查询数据库中是否已存在该商品
     product = get_product_by_pid(pid)
@@ -36,7 +33,7 @@ def jd_crawler():
         # 每两秒查询一次，爬取任务是否完成
         while int(redis_client.get(pid)) > 0:
             time.sleep(2)
-    elif len(product.comments) < WORK_LOAD:
+    else:
         # 说明当前爬取任务正在进行中
         # 每两秒查询一次，爬取任务是否完成
         while int(redis_client.get(pid)) > 0:
@@ -62,7 +59,7 @@ def jd_charts(chart, pid):
     jd_page = JDPage(product)
     chart_type = 1
     if chart == 'bar':
-        jd_page.generate_stacked_bar_charts()
+        jd_page.generate_bar_charts()
     elif chart == 'pie':
         jd_page.generate_pie_charts()
         chart_type = 2
@@ -76,19 +73,8 @@ def jd_charts(chart, pid):
         "dashboard.html",
         pid=pid,
         chart_type=chart_type,
-        myechart=jd_page.page.render_embed(),
-        host=REMOTE_HOST,
-        script_list=jd_page.page.get_js_dependencies()
+        myechart=Markup(jd_page.page.render_embed())
     )
-
-
-@app.route('/captcha')
-def get_captcha():
-    filename = pick_a_random_captcha()
-    code = filename[:4]
-    fp = './static/img/captcha/' + code + '.png'
-    session['captcha'] = code
-    return send_file(fp, mimetype='image/png')
 
 
 @app.errorhandler(404)
